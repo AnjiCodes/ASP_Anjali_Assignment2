51caa8f60e13df04c3d6786c6f608116634f9517
AnjiCodes <n01579648@humber.ca>
2023-03-11 17:49:41 -0500

    J3 changes

--- ASP_Anjali_Assignment2/Controllers/J3Controller.cs
+++ ASP_Anjali_Assignment2/Controllers/J3Controller.cs
@@ -7,7 +7,44 @@ using System.Web.Http;
 
 namespace ASP_Anjali_Assignment2.Controllers
 {
+    //I Have tried J3 (2007) Problem
     public class J3Controller : ApiController
     {
+        [HttpPost]
+        public string CalculateDeal(int a, int[]valuesEliminated, int firstOffer)
+        {
+            int[] DealValues = {100,500,1000,5000,10000,25000,50000,100000,500000,1000000};
+            int total = 0;
+            int counter = 0;
+
+            if (a < 1 || a > 10)
+            {
+                return "Invalid input";
+            }
+            if (firstOffer <=10)
+            {
+                return "Invalid input";
+            }
+                
+            for (int i = 1; i <= 10; i++)
+            {
+                if (!valuesEliminated.Contains(i))
+                {
+                    total += i;
+                    counter++;
+                }
+            }
+            int average = total / counter;
+            if (firstOffer> average) 
+            {
+                return "deal";
+            }
+            else
+            {
+                return "no deal";
+            }
+
+        }
+        
     }
 }
