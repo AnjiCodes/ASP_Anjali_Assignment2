74c1dcb9496cf93707dfa339a86d1d2dfcca17ef
AnjiCodes <n01579648@humber.ca>
2023-03-11 17:51:32 -0500

    few other changes

--- ASP_Anjali_Assignment2/Controllers/J3Controller.cs
+++ ASP_Anjali_Assignment2/Controllers/J3Controller.cs
@@ -10,6 +10,13 @@ namespace ASP_Anjali_Assignment2.Controllers
     //I Have tried J3 (2007) Problem
     public class J3Controller : ApiController
     {
+        /// <summary>
+        /// this method will check whether the user should take the deal or not
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="valuesEliminated"></param>
+        /// <param name="firstOffer"></param>
+        /// <returns>it'll return the average and on basis of that it'll show deal oe no deal</returns>
         [HttpPost]
         public string CalculateDeal(int a, int[]valuesEliminated, int firstOffer)
         {
